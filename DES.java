e720089755b16a36dfe50c9907375d3d60292c13
Satya Ajay <86813260+rsa914016@users.noreply.github.com>
2023-03-15 22:55:43 +0530

    Update DES.java

--- DES.java
+++ DES.java
@@ -1,7 +1,31 @@
 package Cryptography;
 
-public class DES {
-    public static void main(String[] args) {
+import java.security.*;
+import javax.crypto.*;
+import java.util.*;
 
+public class DES
+{
+    public static void main(String[] argv) {
+        try{
+            System.out.println("------ DES Algorithm ------");
+            KeyGenerator keygenerator = KeyGenerator.getInstance("DES");
+            SecretKey myDesKey = keygenerator.generateKey();
+            Cipher desCipher;
+            desCipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+            desCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter Message :");
+            String message = sc.nextLine();
+            byte[] text = message.getBytes();
+            byte[] textEncrypted = desCipher.doFinal(text);
+            System.out.println("Encrypted Message: " + new String(textEncrypted));
+            desCipher.init(Cipher.DECRYPT_MODE, myDesKey);
+            byte[] textDecrypted = desCipher.doFinal(textEncrypted);
+            System.out.println("Decrypted Message: " + new String(textDecrypted));
+        }
+        catch(NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e){
+            e.printStackTrace();
+        }
     }
 }
