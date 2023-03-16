d57894d9319553aafa18abdb4e295f93d810c6b8
Satya Ajay <86813260+rsa914016@users.noreply.github.com>
2023-03-16 09:07:11 +0530

    Update DES.java

--- DES.java
+++ DES.java
@@ -1,30 +1,41 @@
 package Cryptography;
-import java.security.*;
-import java.util.*;
-import javax.crypto.*;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import java.nio.charset.StandardCharsets;
+import javax.crypto.SecretKey;
+import java.util.Scanner;
 
-public class DES
-{
-    public static void main(String[] argv) {
-        try{
-            System.out.println("------ DES Algorithm ------");
-            KeyGenerator keygenerator = KeyGenerator.getInstance("DES");
-            SecretKey myDesKey = keygenerator.generateKey();
-            Cipher desCipher;
-            desCipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
-            desCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Enter Message :");
-            String message = sc.nextLine();
-            byte[] text = message.getBytes();
-            byte[] textEncrypted = desCipher.doFinal(text);
-            System.out.println("Encrypted Message: " + new String(textEncrypted));
-            desCipher.init(Cipher.DECRYPT_MODE, myDesKey);
-            byte[] textDecrypted = desCipher.doFinal(textEncrypted);
-            System.out.println("Decrypted Message: " + new String(textDecrypted));
-        }
-        catch(NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e){
-            e.printStackTrace();
-        }
+
+public class BlowFish {
+    Cipher ecipher,dcipher;
+    BlowFish(SecretKey key) throws Exception {
+        ecipher = Cipher.getInstance("Blowfish");
+        dcipher = Cipher.getInstance("Blowfish");
+        ecipher.init(Cipher.ENCRYPT_MODE, key);
+        dcipher.init(Cipher.DECRYPT_MODE, key);
+    }
+    public String encrypt(String message) throws Exception{
+        byte[] byte_message = message.getBytes(StandardCharsets.UTF_8);
+        byte[] encrypted = ecipher.doFinal(byte_message);
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    public String decrypt(String encrypted) throws Exception {
+        byte[] dec = Base64.getDecoder().decode(encrypted);
+        byte[] decrypted = dcipher.doFinal(dec);
+        return new String(decrypted, StandardCharsets.UTF_8);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Message :");
+        String message = sc.nextLine();
+        SecretKey key = KeyGenerator.getInstance("Blowfish").generateKey();
+        BlowFish obj = new BlowFish(key);
+        String encrypted = obj.encrypt(message);
+        System.out.println("Encrypted Message :" + encrypted);
+        String decrypted = obj.decrypt(encrypted);
+        System.out.println("DecryptedMessage  :" + decrypted);
     }
 }
